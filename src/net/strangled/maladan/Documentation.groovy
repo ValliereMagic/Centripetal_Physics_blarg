c51c31e69bfb300c78f9a832634ba6856a889138
ValliereMagic <ValliereMagic@users.noreply.github.com>
2017-03-07 18:33:03 -0500

    Added feature one: orbital radius.

--- src/net/strangled/maladan/Documentation.groovy
+++ src/net/strangled/maladan/Documentation.groovy
@@ -6,8 +6,11 @@ package net.strangled.maladan
 class Documentation {
     static void printInstructions() {
         print("This is the MaladaN Centripetal Physics calculator. Instructions follow. \n" +
-                "   To get Help using the program: use the parameter -h \n" +
-                "   To calculate orbital radius use --atomic-radius, or -ar \n" +
-                "       ")
+                "   To get Help using the program: use --help or -h \n" +
+                "===========================================================\n" +
+                "   To calculate orbital radius use --orbital-radius, or -or \n" +
+                "       To specify mass use -m (in kilograms)\n" +
+                "       To specify radius use -r (in meters)\n" +
+                "       To specify period use -p (in seconds)\n")
     }
 }
