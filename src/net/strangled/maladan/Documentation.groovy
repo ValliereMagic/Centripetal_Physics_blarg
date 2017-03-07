d431c2e1126e74d997c55c64816f3d8cfd3e02ea
ValliereMagic <louisemelaney@outlook.com>
2017-03-07 17:46:37 -0500

    Initial project upload

---
+++ src/net/strangled/maladan/Documentation.groovy
@@ -0,0 +1,13 @@
+package net.strangled.maladan
+
+/**
+ * MaladaN (R)
+ */
+class Documentation {
+    static void printInstructions() {
+        print("This is the MaladaN Centripetal Physics calculator. Instructions follow. \n" +
+                "   To get Help using the program: use the parameter -h \n" +
+                "   To calculate orbital radius use --atomic-radius, or -ar \n" +
+                "       ")
+    }
+}
