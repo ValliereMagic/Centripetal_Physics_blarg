b29a88605241fc4d30122141da2834a8832e2533
ValliereMagic <ValliereMagic@users.noreply.github.com>
2017-03-07 17:46:37 -0500

    Initial project upload

---
+++ src/net/strangled/maladan/AtomicRadius.groovy
@@ -0,0 +1,10 @@
+package net.strangled.maladan
+
+/**
+ * MaladaN(R)
+ */
+class AtomicRadius {
+    static double execute(one, two, three) {
+
+    }
+}
---
+++ src/net/strangled/maladan/Main.groovy
@@ -0,0 +1,14 @@
+package net.strangled.maladan
+
+/**
+ * MaladaN (R)
+ */
+class Main {
+    static void main(String[] args) {
+        if (args[0] == '--help' || args[0] == '-h') {
+            Documentation.printInstructions()
+        } else if (args[0] == '--atomic-radius' || args[0] == '-ar') {
+            
+        }
+    }
+}
