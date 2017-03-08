1ede20bd76877062dfb7e3146ace7279296edc45
ValliereMagic <ValliereMagic@users.noreply.github.com>
2017-03-08 17:29:06 -0500

    Finished Orbital Radius (I think?)

--- src/net/strangled/maladan/Documentation.groovy
+++ src/net/strangled/maladan/Documentation.groovy
@@ -7,11 +7,17 @@ class Documentation {
     static void printInstructions() {
         print("This is the MaladaN Centripetal Physics calculator. Instructions follow. \n" +
                 "=======================================================================\n" +
+                "Masses must be submitted in meters.\n" +
+                "Velocities must be submitted in meters per second.\n" +
+                "Periods must be submitted in seconds.\n" +
+                "Centripetal Accelerations must be submitted in meters per second squared.\n" +
+                "Frequency must be submitted in hertz\n" +
+                "=========================================================================\n" +
                 "   To get Help using the program: use --help or -h \n" +
                 "===========================================================\n" +
                 "   To calculate orbital radius use --orbital-radius, or -or \n" +
-                "       To specify mass use -m (in kilograms)\n" +
-                "       To specify period use -p (in seconds)\n" +
+                "       To specify the central object's mass use -cm \n" +
+                "       To specify period use -p \n" +
                 "       To specify centripetal acceleration use -cacc\n" +
                 "       To specify frequency use -fr\n" +
                 "       To specify velocity use -v\n" +
