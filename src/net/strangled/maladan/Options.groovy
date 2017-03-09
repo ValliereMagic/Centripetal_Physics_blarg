f137cecd96bf5b08947bc4560457e7d2412dfc60
FRadford <francis.radford@outlook.com>
2017-03-09 13:35:49 -0500

    Added CentripetalAcceleration class

--- src/net/strangled/maladan/Options.groovy
+++ src/net/strangled/maladan/Options.groovy
@@ -5,32 +5,51 @@ package net.strangled.maladan
  */
 class Options {
     private double cMass
+    private double orMass
     private double period
     private double aC
     private double velocity
     private double frequency
     private double orbitalRadius
+    private double forceGravity
+    private int size
 
     Options(LinkedList<String> info) {
         for (int i = 0; i < info.size(); i++) {
             if (info.get(i) == '-cm') {
                 cMass = Double.valueOf(info.get(i + 1))
+                size++
+            } else if (info.get(i) == '-orm') {
+                orMass = Double.valueOf(info.get(i + 1))
+                size++
             } else if (info.get(i) == '-p') {
                 period = Double.valueOf(info.get(i + 1))
+                size++
             } else if (info.get(i) == '-cacc') {
                 aC = Double.valueOf(info.get(i + 1))
+                size++
             } else if (info.get(i) == '-v') {
                 velocity = Double.valueOf(info.get(i + 1))
+                size++
             } else if (info.get(i) == '-fr') {
                 frequency = Double.valueOf(info.get(i + 1))
+                size++
             } else if (info.get(i) == '-or') {
                 orbitalRadius = Double.valueOf(info.get(i + 1))
+                size++
+            } else if (info.get(i) == '-fG') {
+                forceGravity = Double.valueOf(info.get(i + 1))
+                size++
             }
         }
     }
 
     double getcMass() {
         return cMass
+    }
+
+    double getorMass() {
+        return orMass
     }
 
     double getPeriod() {
@@ -51,5 +70,13 @@ class Options {
 
     double getOrbitalRadius() {
         return orbitalRadius
+    }
+
+    double getForceGravity() {
+        return forceGravity
+    }
+
+    int getSize() {
+        return size
     }
 }
